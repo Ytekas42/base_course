f70e77dcf774f17490af37ccb4fe9a3cae71d0e9
Ytekas42 <thebestqw30@gmail.com>
2025-10-09 16:50:46 +0000

    1 лаб и лекц

---
+++ lec_data_type.py
@@ -0,0 +1,23 @@
+type
+
+type(3)
+
+print(type(3))
+
+print(type(3.4))
+
+print(type("Молодец"))
+
+a = "molodec"
+print(type(a))
+
+a = ["molodec", 1, "Kruto", 6, 8]
+print(type(a))
+
+print(type(True))
+
+print(type(False))
+
+print(type(None))
+print(print(3))
+
---
+++ lec_str.py
@@ -0,0 +1,14 @@
+a = "good"
+b = "bad"
+
+print(a + b)
+
+# print(a - b)
+
+print(a * 3)
+
+c = 50
+d = 10
+print(f"Ставим числа {c} and {d}")
+
+print(len(a))
