100d6ee65308406ebfb6210629e644406bde109e
Ytekas42 <thebestqw30@gmail.com>
2025-10-16 15:54:03 +0000

    idad

--- task_1.py
+++ task_1.py
@@ -1,6 +1,7 @@
 print('Hello,World!')
 print("Маша + Петя = Любовь")
 print("x = 3 + 4")
+
 x = 3 + 4
 print("x=", x)
 print(f"x = {x}")
--- task_4.py
+++ task_4.py
@@ -1,8 +1,13 @@
 a = [1, 5, "good", "bad"]
 
 b = [9, "blue", "red", 11]
+
 print(a[1] + b[3])
+
 print(a[2] + b[2])
+
 print(a[0] * b[0])
+
 print(a[1] ** b[3])
+
 print(a + b)
