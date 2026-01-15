4fe8c61aff9f01e40049ac5bf7c26b74796eb1c0
Ytekas42 <thebestqw30@gmail.com>
2026-01-15 16:15:32 +0000

    DDS

---
+++ surface_3d.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig, ax=plt.subplots(subplot_kw={"projection": "3d"})
+
+phi=np.linspace(0, 2*np.pi, 100)
+theta=np.linspace(0, np.pi, 100)
+R=5
+
+x=R*np.outer(np.cos(phi), np.sin(theta))
+y=R*np.outer(np.sin(phi), np.sin(theta))
+z=R*np.outer(np.ones(np.size(phi)), np.cos(theta))
+
+ax.plot_surface(x, y, z, label='Dich')
+
+
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+ax.set_title('3D Test')
+
+plt.savefig('fig_2.png')
